a9f8cca045f16cf1928caceee80497a59919e033
BGagnolo <benjamingagnolo@gmail.com>
2023-05-16 18:29:37 -0300

    template listo

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import * as React from "react";
+//import  NavBar from "../src/components/NavBar/NavBar";
+import  Home  from "../src/components/Home/Home";
+
+function App() {
+  
+
+  return (
+    <>
+    <Home />
+    </>
+  )
+}
+
+export default App
